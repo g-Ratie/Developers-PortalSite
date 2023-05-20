c73183214bdd51f9da7bf60bace731aa758059dd
1F10220146 <s1f102201469@iniad.org>
2023-05-21 03:01:41 +0900

    feat: ログインに応じてメニューを出すように

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { ColorScheme } from '@mantine/core';
 import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
   const toggleColorScheme = () => {
     setColorScheme((scheme) => (scheme === 'light' ? 'dark' : 'light'));
@@ -11,8 +12,10 @@ function App({ Component, pageProps }: AppProps) {
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider>
-        <Component {...pageProps} />
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </MantineProvider>
     </ColorSchemeProvider>
   );
