6ee44d919a96a6dff4a933ecd678c8a2242762b4
1F10220146 <s1f102201469@iniad.org>
2023-05-25 14:05:48 +0900

    feat: ユーザーメニューに遷移を追加

--- src/component/base/header/usermenu.tsx
+++ src/component/base/header/usermenu.tsx
@@ -1,9 +1,18 @@
 import { Avatar, Button, Menu, Text } from '@mantine/core';
-import { IconGraph, IconLogout, IconSettings, IconUser } from '@tabler/icons-react';
+import {
+  IconAddressBook,
+  IconBuilding,
+  IconGraph,
+  IconLogout,
+  IconSettings,
+  IconUsers,
+} from '@tabler/icons-react';
 import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const UserMenu = () => {
   const { data: session } = useSession();
+  const router = useRouter();
   return (
     <Menu shadow="md" width={200}>
       <Menu.Target>
@@ -16,9 +25,25 @@ const UserMenu = () => {
       <Menu.Dropdown>
         <Menu.Item disabled>ログイン中: {session?.user?.name}</Menu.Item>
         <Menu.Divider />
-        <Menu.Item icon={<IconUser size={14} />}>プロフィール</Menu.Item>
-        <Menu.Item icon={<IconGraph size={14} />}>統計データ</Menu.Item>
-        <Menu.Item icon={<IconSettings size={14} />}>設定</Menu.Item>
+        <Menu.Item icon={<IconBuilding size={14} />}>ホーム</Menu.Item>
+        <Menu.Item icon={<IconAddressBook size={14} />}>プロフィール(未実装)</Menu.Item>
+        <Menu.Item
+          icon={<IconGraph size={14} />}
+          onClick={() => {
+            router.push('/stats');
+          }}
+        >
+          統計データ
+        </Menu.Item>
+        <Menu.Item
+          icon={<IconUsers size={14} />}
+          onClick={() => {
+            router.push('/users');
+          }}
+        >
+          メンバーリスト
+        </Menu.Item>
+        <Menu.Item icon={<IconSettings size={14} />}>設定(未実装)</Menu.Item>
         <Menu.Divider />
         <Menu.Item color="red" icon={<IconLogout size={14} />} onClick={() => signOut()}>
           ログアウト
